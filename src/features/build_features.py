a5ecaab8c7c96dce35093fd89ffa5fd68ebb0639
Bekarys Gabdrakhimov <bekarys0504@gmail.com>
2022-10-06 21:17:08 +0200

    First model build

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -9,7 +9,7 @@ from pathlib import Path
 @click.argument('output_filepath', type=click.Path())
 
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
+    """ Runs data processing scripts to turn interim data from (../interim) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
 
