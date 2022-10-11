35eda1f2eeb2f311831ce50fc99cf6e869b1a5b4
Bekarys Gabdrakhimov <bekarys0504@gmail.com>
2022-10-11 19:12:20 +0200

    cheb2 done, more features added

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -20,6 +20,7 @@ from omegaconf import OmegaConf
 from plotly.graph_objs import Annotations, Figure, Layout, Marker, Scatter
 from plotly.graph_objs.layout import Annotation, Font, YAxis
 import os
+from scipy import stats, signal
 
 mne.set_log_level(False)
 # I had error as in this link https://github.com/open-mmlab/mmdetection/issues/7035
@@ -41,10 +42,10 @@ def main(input_filepath, output_filepath):
     logger.info('making final data set from raw data')
 
     save_excel_file = False
-    plot_eeg = False
-    plot_psd = False
-    save_eeg = False
-    save_psd = False
+    plot_eeg = True
+    plot_psd = True
+    save_eeg = True
+    save_psd = True
     extension = '.csv'
     dataset = 'Dataset1'
     layout = read_montage()
@@ -61,6 +62,7 @@ def main(input_filepath, output_filepath):
     # iterate through all .edf files in 'input_filepath'
     for files in all_files:
         new_filename, subject, state, eye_state, subject_class = get_new_filename(files)
+
         filename = Path(files).stem # filename without extension
 
         # read files
@@ -71,14 +73,15 @@ def main(input_filepath, output_filepath):
         
         raw = rename_channels(raw)
         raw.set_montage(layout, on_missing = 'ignore');
-        raw_filtered = filter(raw)
+        raw_filtered = filter_cheb2(raw)
         
+        '''
         # perform ica
         raw_filtered.plot()
         raw_ica, excluded_channels = perform_ica(raw_filtered.copy())
         raw_filtered.plot()
         raw_ica.plot()
-
+        
         # save files
         if os.path.exists(output_filepath+'/'+new_filename+extension):
             print('file exists')
@@ -92,6 +95,7 @@ def main(input_filepath, output_filepath):
         data_df.to_csv(output_filepath+'/'+new_filename+extension)
 
         df = df.append(pd.Series({'fname':filename, 'channels':raw_filtered.get_data().shape[0], 'length':raw_filtered.get_data().shape[1], 'ic_removed':excluded_channels}), ignore_index=True)
+        '''
 
         # plot eeg data and it's power spectral density and save them in figures
         if plot_eeg:
@@ -146,7 +150,7 @@ def check_state(string):
 
 def plot_psd_data(data, config, filename, save, save_folder):
     fig, axes = plt.subplots(figsize = (8,3));
-    data.plot_psd(ax=axes, fmax=100, color = config.colors['dtu_red'], show=False, spatial_colors=False, exclude=['event']);
+    data.plot_psd(ax=axes, fmax=100, color = config.colors['dtu_red'], show=False, spatial_colors=False, exclude=['event', 'bads']);
     
     if save:
         fig.savefig(save_folder + filename +'.png') # save figures     
@@ -207,6 +211,26 @@ def filter(data):
 
     data_filt = data.copy().filter(low_cut, high_cut);
     data_filt = data_filt.copy().notch_filter(50);
+    return data_filt
+
+def filter_cheb2(data):
+    # crop the signal
+    max_seconds = (data.get_data().shape[1]-1)/500 # calculate total time
+    data.crop(tmin=1, tmax=max_seconds-2) # crop end
+
+    fs = config['freq_smapling']
+    BP_EDGE = config.preprocess['BP_EDGE']
+    lower_stop , upper_stop , lower_pass , upper_pass = BP_EDGE
+    ws = [lower_stop/(fs*0.5), upper_stop/(fs*0.5)] # stopband edge frequencies
+    wp = [lower_pass/(fs*0.5), upper_pass/(fs*0.5)] # passband edge frequencies
+    rp = 3 # maximum loss in pb
+    rs = 60 # minimum attenuation in sb
+    N, Wn = signal.cheb2ord(wp,ws,rp,rs)
+    iir_params = dict(ftype='cheby2', order=N, rp=rp, rs=rs, output='sos')
+    iir_params = mne.filter.construct_iir_filter(iir_params, [lower_pass, upper_stop], sfreq=fs, btype='bandpass')  
+
+    raw_cheb = data.copy().filter(lower_pass, upper_stop, iir_params=iir_params, method='iir');
+    data_filt = raw_cheb.copy().notch_filter(50);
     return data_filt
 
 def read_montage(show=False):
--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,6 +3,12 @@ from dotenv import find_dotenv, load_dotenv
 from omegaconf import OmegaConf
 import logging
 from pathlib import Path
+import glob
+import pandas as pd
+import numpy as np
+from scipy import stats
+import nolds
+import antropy as ant
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -17,6 +23,83 @@ def main(input_filepath, output_filepath):
     logger.info('Building features')
 
     
+    all_files = glob.glob('./data/interim/*.csv', recursive=True)
+    all_files = [x for x in all_files if 'post' not in x] # get only pre data
+
+    SEGMENT_LENS = config['epochs']
+    CHANNELS = config['channels']
+    fs = config['freq_smapling']
+
+    for channels in CHANNELS:
+        for segment_len in SEGMENT_LENS:
+            
+            # create dataframe to store all features
+            column_names = get_columns(channels)
+            features_df = pd.DataFrame(columns=column_names)
+            
+            for file in all_files:
+                data = pd.read_csv(file)
+                data = data[channels]
+                subject_class = [0 if 'healthy' in file else 1]
+
+                segments = split_into_segments(data, segment_len, fs)
+
+                for i, segment in enumerate(segments.values()):
+                    features = extract_features(segment, channels)
+                    features_df.loc[len(features_df)] = features+subject_class
+            
+            print('Saving pre_{}_ch_{}s_features.csv'.format(len(channels), segment_len))
+            
+            features_df.to_csv(output_filepath+'/pre_{}_ch_{}s_features.csv'.format(len(channels), segment_len))
+
+# function to split data into segments
+def split_into_segments(df,split_seg_len ,fs):
+    sample_split_len = split_seg_len*fs # samples
+    start_segments = np.arange(0, len(df), sample_split_len) # (start point, stop point, step length)
+    i=0 # counter
+    d = {} # empty
+    for j in start_segments:
+        i+=1
+        d["segment{0}".format(i)] = df.iloc[j:j+sample_split_len ,:]
+    
+    return d
+
+def extract_features(data_df, channels):
+    features = []
+
+    # normalize each channel
+    # data_df=(data_df-data_df.min())/(data_df.max()-data_df.min())
+
+    features = features + [np.mean(np.abs(data_df[ch])) for ch in channels] # absolute mean of each channel
+    features = features + [np.var(data_df[ch]) for ch in channels] # variance of each channel
+    features = features + [max(data_df[ch])  for ch in channels] # peaks
+    features = features + [stats.skew(data_df[ch])  for ch in channels] # skewness
+    features = features + [stats.kurtosis(data_df[ch])  for ch in channels] # kurtosis
+    features = features + [np.sum(np.abs(data_df[ch])**2) for ch in channels] # energy
+    features = features + [np.std(data_df[ch])  for ch in channels] # standard deviation
+    features = features + [nolds.hurst_rs(data_df[ch]) for ch in channels] # Hurst exponent
+    features = features + [ant.sample_entropy(data_df[ch]) for ch in channels] # sample entropy
+    features = features + [ant.hjorth_params(data_df[ch])[0] for ch in channels] # Hjorth mobility
+    features = features + [ant.hjorth_params(data_df[ch])[1] for ch in channels] # Hjorth complexity
+
+    return features
+
+def get_columns(ch_names):
+    mean_names = ["Mean-" + chan for chan in ch_names]
+    var_names = ["Var-" + chan for chan in ch_names]
+    peak_names = ["Peak-" + chan for chan in ch_names]
+    skew_names = ["Skewness-" + chan for chan in ch_names]
+    kurt_names = ["Kurtosis-" + chan for chan in ch_names]
+    energy_names = ["Energy-" + chan for chan in ch_names]
+    std_names = ["Std-" + chan for chan in ch_names]
+    hurst_names = ["Hurst-" + chan for chan in ch_names]
+    se_names = ["SampleEnt-" + chan for chan in ch_names]
+    hjmob_names = ["HjMob-" + chan for chan in ch_names]
+    hjcomp_names = ["HjComp-" + chan for chan in ch_names]
+
+
+    column_names = mean_names+var_names+peak_names+skew_names+kurt_names+energy_names+std_names+hurst_names+se_names+hjmob_names+hjcomp_names+['depressed']
+    return column_names
 
 
 if __name__ == '__main__':
